bdc9d2012a5e97c3ac3c6f0af78b713cc9b09708
Stawerr <renato-silva_16@hotmail.com>
2022-03-23 22:19:03 +0000

    controladores

--- app/Http/Controllers/AudiController.php
+++ app/Http/Controllers/AudiController.php
@@ -8,6 +8,19 @@ class AudiController extends Controller
 {
     public function index()
     {
-        return view('audi.index');
+        $imagesCarousel=[
+            'https://media.gettyimages.com/photos/audi-a4-quattro-sedans-picture-id459005525?s=2048x2048',
+            'https://media.gettyimages.com/photos/audi-a3-quattro-steering-wheel-picture-id157728351?s=2048x2048',
+            'https://media.gettyimages.com/photos/audi-vehicles-on-the-parking-picture-id615509902?s=2048x2048',
+            'https://media.gettyimages.com/photos/white-audi-a6-in-charleston-usa-picture-id459014029?s=2048x2048'
+        ];
+        $imagesText=[
+            'https://media.gettyimages.com/photos/audi-a4-quattro-sedans-picture-id459005525?s=2048x2048',
+            'https://media.gettyimages.com/photos/audi-a4-quattro-sedans-picture-id459005525?s=2048x2048'
+        ];
+        return view('audi.index',[
+            'imagesCarousel'=>$imagesCarousel,
+            'imagesText'=>$imagesText
+        ]);
     }
 }
--- resources/views/audi/index.blade.php
+++ resources/views/audi/index.blade.php
@@ -5,12 +5,8 @@
         <div class="row">
             <div class="col-12">
                 @component('components.carousel.carrousel', [
-                    'images' => [
-                        'https://media.gettyimages.com/photos/audi-a4-quattro-sedans-picture-id459005525?s=2048x2048',
-                        'https://media.gettyimages.com/photos/audi-a3-quattro-steering-wheel-picture-id157728351?s=2048x2048',
-                        'https://media.gettyimages.com/photos/audi-vehicles-on-the-parking-picture-id615509902?s=2048x2048',
-                        'https://media.gettyimages.com/photos/white-audi-a6-in-charleston-usa-picture-id459014029?s=2048x2048'
-                    ]
+                    'images' =>$imagesCarousel
+
                 ])
                 @endcomponent
             </div>
@@ -27,10 +23,7 @@
     <div class="container">
         <div class="row">
                 @component('components.content.contentImage',[
-                    'images'=>[
-                        'https://media.gettyimages.com/photos/audi-a4-quattro-sedans-picture-id459005525?s=2048x2048',
-                        'https://media.gettyimages.com/photos/audi-a4-quattro-sedans-picture-id459005525?s=2048x2048'
-                     ]
+                    'images'=>$imagesText
                 ])
                 @endcomponent
         </div>
